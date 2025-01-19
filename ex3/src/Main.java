378c00ea1fe0d6f724d0fa083e03a845724d8383
Arel Sharon <arel.sharon@gmail.com>
2025-01-19 21:03:06 +0200

    fix ex3

--- ex3/src/Main.java
+++ ex3/src/Main.java
@@ -1,14 +1,12 @@
-   
+
 import java.io.*;
 
 import java_cup.runtime.Symbol;
 import AST.*;
 import SYMBOL_TABLE.SemanticException;
 
-public class Main
-{
-	static public void main(String argv[])
-	{
+public class Main {
+	static public void main(String argv[]) {
 		Lexer l;
 		Parser p;
 		Symbol s;
@@ -17,9 +15,8 @@ public class Main
 		PrintWriter file_writer;
 		String inputFilename = argv[0];
 		String outputFilename = argv[1];
-		
-		try
-		{
+
+		try {
 			/********************************/
 			/* [1] Initialize a file reader */
 			/********************************/
@@ -29,12 +26,12 @@ public class Main
 			/* [2] Initialize a file writer */
 			/********************************/
 			file_writer = new PrintWriter(outputFilename);
-			
+
 			/******************************/
 			/* [3] Initialize a new lexer */
 			/******************************/
 			l = new Lexer(file_reader);
-			
+
 			/*******************************/
 			/* [4] Initialize a new parser */
 			/*******************************/
@@ -43,48 +40,39 @@ public class Main
 			/***********************************/
 			/* [5] 3 ... 2 ... 1 ... Parse !!! */
 			/***********************************/
-			try{
+			try {
 				AST = (AST_PROGRAM) p.parse().value;
 				AST.PrintMe();
 				AST.SemantMeLog();
 				file_writer.write("OK");
-			}
-			catch(LexerError e)
-			{
+			} catch (LexerError e) {
 				System.out.println(e.getMessage());
 				file_writer.write("ERROR");
-			}
-			catch(SemanticException e)
-			{
+			} catch (SemanticException e) {
 				e.printStackTrace();
 				file_writer.write("ERROR(" + (e.lineNumber + 1) + ")\n");
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
-				file_writer.write("ERROR(" + (p.errorLine + 1) + ")\n");
+				file_writer.write("ERROR(" + (p.errorLine) + ")\n");
+			} catch (Throwable e) {
+				file_writer.write("ERROR");
 			}
-			
-			
+
 			/*************************/
 			/* [6] Print the AST ... */
 			/*************************/
-			
+
 			/*************************/
 			/* [7] Close output file */
 			/*************************/
 			file_writer.close();
-			
+
 			/*************************************/
 			/* [8] Finalize AST GRAPHIZ DOT file */
 			/*************************************/
 			AST_GRAPHVIZ.getInstance().finalizeFile();
-    	}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
-
-
